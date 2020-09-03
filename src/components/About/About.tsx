f957ea6a6e96597f3e1c3b51a7ecf350215441f2
doylea <doyley.annmarie@gmail.com>
2020-09-02 22:24:19 -0400

    Remove starter text

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -24,30 +24,7 @@ const About: React.FunctionComponent<AboutProps> = ({
       <Description>
         <h4>Hey there my friend!</h4>
         <p>
-          My name is {author.name}, and I am {yearsSince(author.birthDate)}{" "}
-          years old. I have a passion for products having constructive effects
-          on our lives, and I love to be involved in the development of
-          purposeful applications.
-        </p>
-
-        <p>
-          I contribute to the product life cycle with my comprehensive skills in
-          web development, my growing knowledge of behavioral sciences and human
-          psychology, and my neverending hunger for growth.
-        </p>
-
-        <p>
-          When not building stuff, I love to explore, read fascinating books,
-          learn languages, write about my experiences in life, have amazing
-          dinners and meet interesting people. I am also obsessed with building
-          positive habits. I believe they are the keys to unlock our full
-          potential.
-        </p>
-
-        <p>
-          Do you want to find out more about me? Please shoot me an email at{" "}
-          <strong>{author.social.email}</strong> or reach out to me on social
-          media, listed below.
+          My name is {author.name}.
         </p>
       </Description>
     </Root>
